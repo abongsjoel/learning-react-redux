e54c8f92b3d68f0aff2221110a92b976a13703c8
tj-ubuntu <abongsjoel@gmail.com>
2021-07-12 18:03:00 +0100

    Add hardcoded Muffins

---
+++ src/components/Muffins/Muffins.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Muffins = () => {
+  return (
+    <ul>
+      <li>Chocolate chip Muffin</li>
+      <li>Blueberry Muffin</li>
+      <li>Banana Muffin</li>
+      <li>BBQ Meat Lovers Muffin</li>
+    </ul>
+  )
+}
+
+export default Muffins;
