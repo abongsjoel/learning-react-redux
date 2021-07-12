ae4afdaa8a653d20c0faf65577c9112c999a1bb6
tj-ubuntu <abongsjoel@gmail.com>
2021-07-12 20:23:37 +0100

    Load muffins from fake server using Redux-Thunk

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,1 +1,3 @@
 export const selectedMuffinsArray = (state) => state.muffins;
+export const selectMuffinsLoading = (state) => state.selectMuffinsLoading;
+export const selectMuffinsLoadError = (state) => state.error;
